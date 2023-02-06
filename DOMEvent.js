f7ef40f06231ee8bfbc46af37545bc40acee2a88
takinabradley <bradfinney98@gmail.com>
2023-02-06 16:38:08 -0500

    add options object to event constructor

--- DOMEvent.js
+++ DOMEvent.js
@@ -1,14 +1,23 @@
 class DOMEvent {
   #stop = false
   #stopImmediately = false
+  #bubbles = false
+  #cancelable = false
 
-  constructor(type) {
+  constructor(type, options) {
     this.type = type
     this.target = null
+
+    if(options) {
+      if(options.bubbles === true) this.#bubbles = true
+      if(options.cancelable === true) this.#cancelable = true
+    }
   }
 
   get stop() {return this.#stop}
   get stopImmediately() {return this.#stopImmediately}
+  get bubbles() {return this.#bubbles}
+  get cancelable() {return this.#cancelable}
 
   stopPropagation() {this.#stop = true}
   stopImmediatePropagation() {
