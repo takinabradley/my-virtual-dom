fe1bb82eb17bf9548e5a90223c6d663a882fd6b2
takinabradley <bradfinney98@gmail.com>
2023-02-08 20:25:28 -0500

    Show off preventDefault() functionality

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-import DOMEvent from './DOMEvent.js'
+import DOMEvent, { CustomDOMEvent } from './DOMEvent.js'
 import FakeDocument from './FakeDocument.js'
 
 /* fakeDocument stuff */
@@ -6,14 +6,16 @@ const fakeDocument = new FakeDocument()
 const div = fakeDocument.createElement('div')
 const p = fakeDocument.createElement('p')
 const span = fakeDocument.createElement('span')
+const span2 = fakeDocument.createElement('span')
+span2.textContent = 'span2'
 fakeDocument.body.appendChild(div)
 div.appendChild(p)
 p.appendChild(span)
+p.appendChild(span2)
 
 function divBubbleHandler(e) {
   console.log('grandparent click bubble')
 }
-
 // capture listeners
 div.addEventListener('click', e => {
   console.log('grandparent click capture')
@@ -27,7 +29,9 @@ p.addEventListener('click', e => {
 span.addEventListener('click', function(e) {
   e.stopImmediatePropagation()       // stopped propogation immediately here!
   console.log('click capture')
+  console.log(e.detail)
   console.log("The element tha immediately stopped propogation was", this)
+  
 }, true)
 
 span.addEventListener('click', e => {
@@ -47,5 +51,21 @@ div.addEventListener('click', divBubbleHandler)
 div.removeEventListener('click', divBubbleHandler)
 
 // click 
-span.click()
-span.dispatchEvent(new DOMEvent('click'))
+
+const customEvent = new CustomDOMEvent('click', {detail: "I'm a detail!"})
+console.log({...customEvent})
+span.dispatchEvent(customEvent)
+
+
+console.log({...customEvent})
+
+const input = fakeDocument.createElement('input')
+fakeDocument.body.addEventListener('keydown', e => {
+  e.preventDefault()
+  console.log('keydown event: prevented default')
+})
+fakeDocument.body.appendChild(input)
+console.log({...input})
+input.keyDown('k')
+input.keyDown(' ')
+console.log({...input})
